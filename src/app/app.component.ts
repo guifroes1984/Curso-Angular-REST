6a8fbdb1a1e1a7bbc240ed5c6cc306fdd4b861dd
guifroes1984 <guilhermefroes26@gmail.com>
2023-01-12 16:21:32 -0300

    Criando barra de menu.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { LoginServiceService } from './service/login-service.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -7,8 +7,25 @@ import { LoginServiceService } from './service/login-service.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Curso-Angular-REST';
 
+  constructor(private router: Router) { 
+
+  }
+
+  ngOnInit(): void {
+
+    if (localStorage.getItem('token') == null) {
+      this.router.navigate(['login']);
+    }
+
+  }
+
+  public sair() {
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
 
 }
